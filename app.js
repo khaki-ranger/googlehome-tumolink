022712c52e4c938b98e3d4437e0262239cd416f9
Yohei Terashima <yamanogomora@gmail.com>
2018-12-04 11:00:10 +0000

    request モジュールで tumolink に POST 送信して、レスポンスを受ける

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const settings = require('./settings');
+const request = require('request');
+
+request.post({
+  url: settings.target.url,
+  form: {spaceId: settings.target.spaceId}
+}, function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
