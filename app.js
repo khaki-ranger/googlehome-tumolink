a630366f99ceffec9311b63d1f40ea9f04d50d0d
Yohei Terashima <yamanogomora@gmail.com>
2018-12-05 00:39:20 +0000

    node-cron で定期実行

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 
 const settings = require('./settings');
 const request = require('request');
+const cron = require('node-cron');
 
-request.post({
-  url: settings.target.url,
-  form: {spaceId: settings.target.spaceId}
-}, function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+cron.schedule('* * * * *', () => {
+  request.post({
+    url: settings.target.url,
+    form: {spaceId: settings.target.spaceId}
+  }, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  });
 });
